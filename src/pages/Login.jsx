2747e9bef68cb6d5acb4ffd4869ff431c99c8f17
SowmithSripadi <sowmithsripadi1@gmail.com>
2024-12-19 22:04:30 -0500

    added testbutton and write components

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,36 +1,12 @@
-import { SignIn, useSignIn } from "@clerk/clerk-react";
+import { SignIn } from "@clerk/clerk-react";
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import TestUserLogin from "./TestUserLogin";
 
 function Login() {
-  const { signIn } = useSignIn();
-  const emailAddress = "test@gmail.com";
-  const password = "12345678";
-  const navigate = useNavigate();
-
-  const handleTestUserLogin = async () => {
-    try {
-      const signInAttempt = await signIn.create({
-        identifier: emailAddress,
-        password,
-      });
-      if (signInAttempt.status === "complete") {
-        navigate("/");
-        location.reload();
-      }
-    } catch (e) {
-      console.error(e);
-    }
-  };
   return (
     <div className="flex flex-col gap-8 justify-center items-center h-[calc(100vh-80px)]">
       <SignIn signUpUrl="/register" />
-      <button
-        onClick={handleTestUserLogin}
-        className="bg-secondaryColor  text-black px-4 py-2 rounded-lg border-2 border-gray-400"
-      >
-        Login as test user
-      </button>
+      <TestUserLogin redirectLocation={"/"} />
     </div>
   );
 }
--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,6 +1,7 @@
 import { useUser } from "@clerk/clerk-react";
 import React from "react";
 import Loading from "../pages/Loading";
+import TestUserLogin from "./TestUserLogin";
 
 function Write() {
   const { isLoaded, isSignedIn } = useUser();
@@ -11,12 +12,45 @@ function Write() {
 
   if (isLoaded && !isSignedIn)
     return (
-      <div className=" mt-40 font-bold text-2xl w-full h-full flex text-center justify-center">
-        You should Log in
+      <div className="flex flex-col items-center gap-8">
+        <div className=" mt-40 font-bold text-2xl w-full h-full flex text-center justify-center">
+          You should Log in
+        </div>
+        <TestUserLogin redirectLocation={"/write"} />
       </div>
     );
 
-  return <div>Write</div>;
+  return (
+    <div className="">
+      <h1 className="mb-6">Create a new Post</h1>
+      <form className="flex flex-col items-start gap-6">
+        <button className="bg-secondaryColor text-black px-4 py-1 rounded-lg border-2 border-gray-400 ">
+          Add a cover image
+        </button>
+        <input
+          placeholder="Write a title here..."
+          className="w-full dark:bg-black text-3xl focus:outline-none focus:ring-0"
+        />
+        <div className="flex gap-4 items-center">
+          <label htmlFor="">Choose a category:</label>
+          <select
+            name="category"
+            id=""
+            className="dark:bg-gray-800 bg-gray-200 dark:border-2 dark:border-gray-300 p-1 rounded-lg"
+          >
+            <option value="Health">Health</option>
+            <option value="Fitness">Fitness</option>
+            <option value="Skin Care">Skin Care</option>
+          </select>
+        </div>
+        <textarea
+          name="desc"
+          placeholder="A short description"
+          className="w-full p-2 rounded-lg dark:bg-gray-700"
+        />
+      </form>
+    </div>
+  );
 }
 
 export default Write;
