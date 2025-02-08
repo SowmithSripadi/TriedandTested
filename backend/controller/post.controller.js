a455ef1ed3c42e14a5b7b675c22290e23f818990
SowmithSripadi <sowmithsripadi1@gmail.com>
2025-02-08 11:09:34 -0500

    added error handling

--- backend/controller/post.controller.js
+++ backend/controller/post.controller.js
@@ -16,7 +16,11 @@ export const createPost = async (req, res) => {
   if (!img || !title || !slug || !desc || !content || !isFeatured || !visit) {
     return res.status(400).json({ message: "All fields are required" });
   }
-  const newPost = new Post(req.body);
-  const post = await newPost.save();
-  res.status(200).json({ message: "Post received", data: post });
+  try {
+    const newPost = new Post(req.body);
+    const post = await newPost.save();
+    res.status(200).json({ message: "Post received", data: post });
+  } catch (err) {
+    res.status(500).json({ message: "Error", error: err });
+  }
 };
