6ef78d03aad320ebe12ff7758163689c9ccbfc91
SowmithSripadi <sowmithsripadi1@gmail.com>
2025-02-15 12:25:34 -0500

    clerkController added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,6 +8,7 @@ import {
   SignedOut,
   SignedIn,
   UserButton,
+  useAuth,
 } from "@clerk/clerk-react";
 import { MdDarkMode } from "react-icons/md";
 import DarkModeButton from "./DarkModeButton";
@@ -16,10 +17,13 @@ function Navbar() {
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
   const [open, setOpen] = useState(false);
   const [isChecked, setIsChecked] = useState(
-    localStorage.getItem("theme") === "dark",
+    localStorage.getItem("theme") === "dark"
   );
 
+  const { getToken } = useAuth();
+
   useEffect(() => {
+    getToken().then((token) => console.log(token));
     const root = window.document.documentElement;
     if (theme === "dark") {
       root.classList.add("dark");
